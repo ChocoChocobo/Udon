f2d8af29dc7bb2f55a69a584d17b3e0f6f341e65
ChocoChocobo <sharonovjr@yahoo.com>
2023-03-08 23:20:32 +0300

    Add project files.

---
+++ Udon/Journal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Udon
+{
+    class Journal
+    {
+        string _name;
+        public string Name
+        {
+            get { return _name != null ? _name : "JournalDefaultName"; }
+            set { _name = value; }
+        }
+
+        int _foundDate;
+        public  int FoundDate 
+        {
+            get { return _foundDate; }
+            set { _foundDate = (value < 1900) ? 2000 : value; }
+        }
+
+        string _description;
+        public string Description
+        {
+            get { return _description != null ? _description : "Just a description"; }
+            set { _description = value; }
+        }
+
+        public string _phoneNumber;
+        public string PhoneNumber
+        {
+            get { return _phoneNumber != null ? _phoneNumber : "+71234567890"; }
+            set { _phoneNumber = value; }
+        }
+
+        public string _email;
+        public string Email
+        {
+            get { return _email != null ? _email : "defaultemail@yahoo.com"; }
+            set { _email = value; }
+        }
+
+        public void Print()
+        {
+            WriteLine($"Name: {Name}\nFoundation date: {FoundDate}\nDescription: {Description}\n" +
+                $"Phone Number: {PhoneNumber}\nEmail: {Email}\n");
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Journal jrl1 = new Journal();
+
+            Journal jrl2 = new Journal
+            {
+                Name = "Best Journal",
+                FoundDate = 2011,
+                Description = "You`ve opened the best journal ever!",
+                PhoneNumber = "+75648679167",
+                Email = "bestjournal@yahoo.com"
+            };
+
+            jrl1.Print();
+            jrl2.Print();
+        }
+    }
+}
---
+++ Udon/Web.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Udon
+{
+    class Web
+    {
+        static string _name;
+        public static string Name
+        {
+            get { return _name != null ? _name : "Just a website";}
+            set { _name = value; }
+        }
+
+        static string _url;
+        public static string URL
+        {
+            get { return _url != null ? _url : "https://github.com/ChocoChocobo"; }
+            set { _url = value; }
+        }
+
+        static string _description;
+        public static string Description
+        {
+            get { return _description != null ? _description : "You should`ve entered description!!!"; }
+            set { _description = value; }
+        }
+
+        static string _ip;
+        public static string IP
+        {
+            get { return _ip != null ? _ip : "8.8.8.8"; }
+            set { _ip = value; }
+        }
+
+        public void Print()
+        {
+            WriteLine($"Website name: {Name}\nURL: {URL}\nDescription: {Description}\nIP: {IP}\n");
+        }
+    }
+
+    internal class Website
+    {
+        static void Main(string[] args)
+        {
+            /*Web website1 = new Web();
+
+            Web website2 = new Web
+            {
+                Name = "Primitive server",
+                URL = "myserver.com",
+                Description = "Using props",
+                IP = "8.8.8.8"
+            };
+            website1.Print();
+            website2.Print();*/
+
+            WriteLine("Welcome to \"Primitive site builder v1!\"");
+            Web website = new Web();
+            Write("Name of your website: ");
+            Web.Name = ReadLine();
+            Write("URL of your website: ");
+            Web.URL = ReadLine();
+            Write("Description of your website: ");
+            Web.Description= ReadLine();
+            Write("IP: ");
+            Web.IP = ReadLine();
+            Write("\n");
+
+            WriteLine("Here is your website!");
+            website.Print();
+
+       
+        }
+    }
+}
